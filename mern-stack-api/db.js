504e3ed165bbff48c1c330330981ebf750e79775
arsham-azami <63233235+arsham-azami@users.noreply.github.com>
2020-11-14 20:57:00 +0330

    adding async and await in db.js file

--- mern-stack-api/db.js
+++ mern-stack-api/db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/postManagerDB',{useNewUrlParser:true,useUnifiedTopology:true},
-    err => {
-        if (!err)
-            console.log('Mongodb connection succeeded.')
-        else
-            console.log('Error while connecting MongoDB : ' + JSON.stringify(err, undefined, 2))
-    })
+async function connecting() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/postManagerDB', { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log('Mongodb connection succeeded.')
+    } catch (err) {
+        console.log('Error while connecting MongoDB : ' + JSON.stringify(err, undefined, 2))
+
+    }
+}
+
+connecting()

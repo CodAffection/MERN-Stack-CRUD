9c87a6ad7bad4f74a897576024f2919add9fbe07
arsham-azami <63233235+arsham-azami@users.noreply.github.com>
2020-11-14 21:10:11 +0330

    using prettier extension  to format index.js

--- mern-stack-api/index.js
+++ mern-stack-api/index.js
@@ -8,8 +8,8 @@ var postMessageRoutes = require('./controllers/postMessageController')
 
 var app = express()
 app.use(bodyParser.json())
-app.use(cors({origin:'http://localhost:3000'}))
-app.listen(4000,()=>console.log('Server started at : 4000'))
+app.use(cors({ origin: 'http://localhost:3000' }))
+app.listen(4000, () => console.log('Server started at : 4000'))
 
 
-app.use('/postMessages',postMessageRoutes)
+app.use('/postMessages', postMessageRoutes)
